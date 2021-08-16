54cd7a9c362ee9797e772a246fff588471407fa2
xiaoxiaoguai <1144822034@qq.com>
2021-08-16 15:40:38 +0800

    修复行为获取不到的Bug

--- Survivalcraft/Subsystem/SubsystemBlockBehaviors.cs
+++ Survivalcraft/Subsystem/SubsystemBlockBehaviors.cs
@@ -8,20 +8,47 @@ namespace Game
 {
     public class SubsystemBlockBehaviors : Subsystem
     {
-        public SubsystemBlockBehavior[][] m_blockBehaviorsByContents;
-
         public List<SubsystemBlockBehavior> m_blockBehaviors = new List<SubsystemBlockBehavior>();
+
+        public Dictionary<int, List<SubsystemBlockBehavior>> m_blockBehaviorsbyid = new Dictionary<int, List<SubsystemBlockBehavior>>();
 
         public ReadOnlyList<SubsystemBlockBehavior> BlockBehaviors => new ReadOnlyList<SubsystemBlockBehavior>(m_blockBehaviors);
 
         public SubsystemBlockBehavior[] GetBlockBehaviors(int value)
         {
-            return BlocksManager.Blocks[Terrain.ExtractContents(value)].GetBehaviors(Project,value).ToArray();
+            int id = Terrain.ExtractContents(value);
+            List<SubsystemBlockBehavior> behaviors = BlocksManager.Blocks[id].GetBehaviors(Project, value);
+            if (m_blockBehaviorsbyid.TryGetValue(id, out List<SubsystemBlockBehavior> bs))
+            {
+                foreach (var c in bs)
+                {
+                    if (!behaviors.Contains(c)) behaviors.Add(c);
+                }
+
+            }
+            return behaviors.ToArray();
         }
         public override void Load(ValuesDictionary valuesDictionary)
         {
             foreach (var subsystem in Project.Subsystems) {
-                if (subsystem is SubsystemBlockBehavior) m_blockBehaviors.Add(subsystem as SubsystemBlockBehavior);
+                if (subsystem is SubsystemBlockBehavior)
+                {
+                    SubsystemBlockBehavior behavior = subsystem as SubsystemBlockBehavior;
+                    foreach (int id in behavior.HandledBlocks)
+                    {
+                        if (m_blockBehaviorsbyid.TryGetValue(id, out List<SubsystemBlockBehavior> bs))
+                        {
+                            if (!bs.Contains(behavior)) bs.Add(behavior);
+                        }
+                        else
+                        {
+                            bs = new List<SubsystemBlockBehavior>();
+                            bs.Add(behavior);
+                            m_blockBehaviorsbyid.Add(id, bs);
+                        }
+                    }
+                    m_blockBehaviors.Add(behavior);
+                }
             }
         }
     }
