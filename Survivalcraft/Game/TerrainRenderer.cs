fc498232f4826b75525edd6a898177e00330b054
xiaoxiaoguai <1144822034@qq.com>
2021-09-21 00:31:17 +0800

    恢复设置shader参数和地形生成的Hook

--- Survivalcraft/Game/TerrainRenderer.cs
+++ Survivalcraft/Game/TerrainRenderer.cs
@@ -160,6 +160,7 @@ namespace Game
 			OpaqueShader.GetParameter("u_fogYMultiplier").SetValue(m_subsystemSky.VisibilityRangeYMultiplier);
 			OpaqueShader.GetParameter("u_fogColor").SetValue(new Vector3(m_subsystemSky.ViewFogColor));
 			ShaderParameter parameter = OpaqueShader.GetParameter("u_fogStartInvLength");
+			ModsManager.HookAction("SetShaderParameter", (modLoader)=> { modLoader.SetShaderParameter(OpaqueShader);return true; });
 			for (int i = 0; i < m_chunksToDraw.Count; i++)
 			{
 				TerrainChunk terrainChunk = m_chunksToDraw[i];
@@ -205,6 +206,8 @@ namespace Game
 			AlphatestedShader.GetParameter("u_fogYMultiplier").SetValue(m_subsystemSky.VisibilityRangeYMultiplier);
 			AlphatestedShader.GetParameter("u_fogColor").SetValue(new Vector3(m_subsystemSky.ViewFogColor));
 			ShaderParameter parameter = AlphatestedShader.GetParameter("u_fogStartInvLength");
+			ModsManager.HookAction("SetShaderParameter", (modLoader) => { modLoader.SetShaderParameter(AlphatestedShader); return true; });
+
 			for (int i = 0; i < m_chunksToDraw.Count; i++)
 			{
 				TerrainChunk terrainChunk = m_chunksToDraw[i];
@@ -233,6 +236,8 @@ namespace Game
 			TransparentShader.GetParameter("u_fogYMultiplier").SetValue(m_subsystemSky.VisibilityRangeYMultiplier);
 			TransparentShader.GetParameter("u_fogColor").SetValue(new Vector3(m_subsystemSky.ViewFogColor));
 			ShaderParameter parameter = TransparentShader.GetParameter("u_fogStartInvLength");
+			ModsManager.HookAction("SetShaderParameter", (modLoader) => { modLoader.SetShaderParameter(TransparentShader); return true; });
+
 			for (int i = 0; i < m_chunksToDraw.Count; i++)
 			{
 				TerrainChunk terrainChunk = m_chunksToDraw[i];
--- Survivalcraft/Game/TerrainUpdater.cs
+++ Survivalcraft/Game/TerrainUpdater.cs
@@ -738,6 +738,8 @@ namespace Game
 						double realTime14 = Time.RealTime;
 						m_statistics.ContentsCount3++;
 						m_statistics.ContentsTime3 += realTime14 - realTime13;
+						ModsManager.HookAction("OnTerrainContentsGenerated", (modLoader) => { modLoader.OnTerrainContentsGenerated(chunk); return true; });
+
 						break;
 					}
 				case TerrainChunkState.InvalidContents4:
