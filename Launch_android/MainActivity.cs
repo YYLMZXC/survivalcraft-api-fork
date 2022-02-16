f8947b74d199c3cb9e289c2669eb836032006944
xiaoxiaoguai <1144822034@qq.com>
2022-02-16 10:45:14 +0800

    修改应用名称和AssemblyTitle

--- Launch_android/MainActivity.cs
+++ Launch_android/MainActivity.cs
@@ -11,7 +11,7 @@ using Engine;
 using Game;
 namespace SC.Android
 {
-    [Activity(Label = "生存战争2.2插件版", LaunchMode = LaunchMode.SingleTask, Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Label = "生存战争2.3", LaunchMode = LaunchMode.SingleTask, Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     [IntentFilter(new string[] { "android.intent.action.VIEW" }, DataScheme = "com.candy.survivalcraft", Categories = new string[] { "android.intent.category.DEFAULT", "android.intent.category.BROWSABLE" })]
 
     public class MainActivity : EngineActivity
