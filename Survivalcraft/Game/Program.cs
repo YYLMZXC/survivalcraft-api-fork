0c7ea26858621a1fd615aebbbdbec183b28135e8
把红色赋予黑海 <txs20061216@126.com>
2023-06-30 22:15:28 +0800

    修改窗口大小

--- Survivalcraft/Game/Program.cs
+++ Survivalcraft/Game/Program.cs
@@ -56,7 +56,7 @@ namespace Game
                 e.IsHandled = true;
             };
             JsInterface.Initiate();
-            Window.Run(480, 320, WindowMode.Resizable, "生存战争2.3插件版V" + ModsManager.APIVersion);
+            Window.Run((int)(wide_init / 1.25f), (int)(high_init * 0.8f), WindowMode.Resizable, "生存战争2.3插件版V" + ModsManager.APIVersion);
         }
 
         public static void HandleUriHandler(Uri uri)
