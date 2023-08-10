aa5a93ec9bbd757b96657ebaa16bd54ff70e766a
把红色赋予黑海 <txs20061216@126.com>
2023-08-10 19:52:59 +0800

    改if else if为switch case

--- AdrasteaLoader/adrastea.cs
+++ AdrasteaLoader/adrastea.cs
@@ -3,26 +3,22 @@
 	public class AdrasteaLoader
 	{
 	
-		public string 点击方块的部位()
+		public string 点击方块的部位(int part)
 		{
-			if (false)
+			switch (part)
 			{
-				return "click_uppart";
+				case 0:
+					return "click_uppart";
+				case 1:
+					return "click_downpart";
+				case 2:
+					return "click_leftpart";
+				case 3: 
+					return"click_rightpart ";
+				default:
+					return null;
 			}
-			else if(false)
-			{
-				return "click_downpart";
 			}
-			else if(false)
-			{
-				return "click_leftpart";
-			}
-			else if(false)
-			{
-				return "click_rightpart ";
-			}
-			return null;
-		}
 	}
 	public class SoarLib_UNIpowerAPI
 	{
