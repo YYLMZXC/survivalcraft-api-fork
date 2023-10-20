662b8e2eefa62543e05936c719f839694c77ff56
把红色赋予黑海 <txs20061216@126.com>
2023-10-20 20:19:22 +0800

    修复安卓14权限

--- Launch_android/MainActivity.cs
+++ Launch_android/MainActivity.cs
@@ -8,12 +8,9 @@ using Engine;
 using Game;
 using System;
 using System.IO;
-using System.Threading.Tasks;
 using Android.Provider;
-using Android.Widget;
-using Xamarin.Essentials;
-using Permissions = Xamarin.Essentials.Permissions;
 using Android;
+using Environment = Android.OS.Environment;
 namespace SC4Android
 {
 	[Activity(Label = "生存战争2.3插件版", LaunchMode = LaunchMode.SingleTask, Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
@@ -21,53 +18,40 @@ namespace SC4Android
 
 	public class MainActivity : EngineActivity
 	{
+		private async void CheckAndRequestPermissions()
+		{
+			if (((int)Build.VERSION.SdkInt) >= (int)BuildVersionCodes.R)
+			{
+				//当版本大于安卓11时
+				if (!Environment.IsExternalStorageManager)
+				{
+					StartActivity(new Intent(Settings.ActionManageAllFilesAccessPermission));
+				}
+			}
+			else if ((int)Build.VERSION.SdkInt >= (int)BuildVersionCodes.M)
+			{
+				bool permissionGranted = true;
+				//当版本大于安卓6
+				var readPermissionStatus = CheckSelfPermission(Manifest.Permission.ReadExternalStorage);
+				if (readPermissionStatus != Permission.Granted)
+				{
+					permissionGranted = false;
+					RequestPermissions(new string[] { Manifest.Permission.ReadExternalStorage }, 0);
+				}
+
+				var writePermissionStatus = CheckSelfPermission(Manifest.Permission.WriteExternalStorage);
+				if (writePermissionStatus != Permission.Granted)
+				{
+					permissionGranted = false;
+					RequestPermissions(new string[] { Manifest.Permission.WriteExternalStorage }, 1);
+				}
+			}
+		}
 		protected override async void OnCreate(Bundle savedInstanceState)
 		{
 			base.OnCreate(savedInstanceState);
-			try
-			{
-				var status_request1 = await Permissions.CheckStatusAsync<Permissions.StorageWrite>();
-				var status_request2 = await Permissions.CheckStatusAsync<Permissions.StorageRead>();
-				if (status_request1 != PermissionStatus.Granted || status_request2 != PermissionStatus.Granted || CheckSelfPermission(Manifest.Permission.WriteExternalStorage) != Permission.Granted)
-				{
-					Toast.MakeText(this, "请授权游戏存储读写权限", ToastLength.Long).Show();
-					RequestPermissions(new string[] { Manifest.Permission.WriteExternalStorage }, 0);
-					RequestPermissions(new string[] { Manifest.Permission.ReadExternalStorage }, 0);
-					RequestPermissions(new string[] { Manifest.Permission.ManageExternalStorage }, 0);
-					if (await Permissions.RequestAsync<Permissions.StorageWrite>() != PermissionStatus.Granted ||
-						await Permissions.RequestAsync<Permissions.StorageRead>() != PermissionStatus.Granted)
-					{
-						Toast.MakeText(this, "请到应用设置里授权：允许\"读取本机存储\"", ToastLength.Long).Show();
-						Toast.MakeText(this, "应用权限不足-01", ToastLength.Long);
-						throw new Exception("应用权限不足-01");
-					}
-					if ((Convert.ToInt32(Build.VERSION.Release) >= 11) && !Android.OS.Environment.IsExternalStorageManager)
-					{
-						Toast.MakeText(this, "Android11以上无法直接访问\n需要手动授权，请在稍后的页面中 创建SCNEXT目录进入并点击\"选择\"按钮", ToastLength.Long).Show();
-						var intent = new Intent(Intent.ActionOpenDocumentTree);
-						intent.AddFlags(ActivityFlags.GrantWriteUriPermission);
-						StartActivity(intent);
-						if ((int)Build.VERSION.SdkInt >= (int)BuildVersionCodes.O)
-						{
-							await Task.Run(() => intent.PutExtra(DocumentsContract.ExtraInitialUri, "/sdcard/"));
-						}
-					}
-				}
-				else
-					Run();
-			}
-			//var status = await Permissions.CheckStatusAsync<Permissions.StorageWrite>();
-			//if (status == PermissionStatus.Granted)
-			//{
-			//	return;
-			//}
-			//Toast.MakeText(this, "权限需要\", \"此软件需要权限以更改Config文件", ToastLength.Long).Show();
-			catch (Exception e)
-			{
-				Toast.MakeText(this, "应用权限不足-02", ToastLength.Long);
-				Log.Error(e);
-				throw new Exception("应用权限不足-02");
-			}
+			CheckAndRequestPermissions();
+			Run();
 		}
 		public void Run()
 		{
