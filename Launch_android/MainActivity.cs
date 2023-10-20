87a31fe6801d0db6b1036d7bd4907dc51e8490d1
把红色赋予黑海 <txs20061216@126.com>
2023-10-20 18:46:09 +0800

    对权限申请排序

--- Launch_android/MainActivity.cs
+++ Launch_android/MainActivity.cs
@@ -13,6 +13,7 @@ using Android.Provider;
 using Android.Widget;
 using Xamarin.Essentials;
 using Permissions = Xamarin.Essentials.Permissions;
+using Android;
 namespace SC4Android
 {
 	[Activity(Label = "生存战争2.3插件版", LaunchMode = LaunchMode.SingleTask, Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
@@ -25,35 +26,47 @@ namespace SC4Android
 			base.OnCreate(savedInstanceState);
 			try
 			{
-				if ((Convert.ToInt32(Build.VERSION.Release) >= 11) && !Android.OS.Environment.IsExternalStorageManager)
+				var status_request1 = await Permissions.CheckStatusAsync<Permissions.StorageWrite>();
+				var status_request2 = await Permissions.CheckStatusAsync<Permissions.StorageRead>();
+				if (status_request1 != PermissionStatus.Granted || status_request2 != PermissionStatus.Granted || CheckSelfPermission(Manifest.Permission.WriteExternalStorage) != Permission.Granted)
 				{
-					Toast.MakeText(this, "Android11以上无法直接访问\n需要手动授权，请在稍后的页面中 选择Android/data目录并点击\"选择\"按钮", ToastLength.Long).Show();
-					var intent = new Intent(Intent.ActionOpenDocumentTree);
-					intent.AddFlags(ActivityFlags.GrantWriteUriPermission);
-					StartActivity(intent);
-					if ((int)Build.VERSION.SdkInt >= (int)BuildVersionCodes.O)
+					Toast.MakeText(this, "请授权游戏存储读写权限", ToastLength.Long).Show();
+					RequestPermissions(new string[] { Manifest.Permission.WriteExternalStorage }, 0);
+					RequestPermissions(new string[] { Manifest.Permission.ReadExternalStorage }, 0);
+					RequestPermissions(new string[] { Manifest.Permission.ManageExternalStorage }, 0);
+					if (await Permissions.RequestAsync<Permissions.StorageWrite>() != PermissionStatus.Granted ||
+						await Permissions.RequestAsync<Permissions.StorageRead>() != PermissionStatus.Granted)
 					{
-						await Task.Run(() => intent.PutExtra(DocumentsContract.ExtraInitialUri, "/sdcard/"));
+						Toast.MakeText(this, "请到应用设置里授权：允许\"读取本机存储\"", ToastLength.Long).Show();
+						Toast.MakeText(this, "应用权限不足-01", ToastLength.Long);
+						throw new Exception("应用权限不足-01");
+					}
+					if ((Convert.ToInt32(Build.VERSION.Release) >= 11) && !Android.OS.Environment.IsExternalStorageManager)
+					{
+						Toast.MakeText(this, "Android11以上无法直接访问\n需要手动授权，请在稍后的页面中 创建SCNEXT目录进入并点击\"选择\"按钮", ToastLength.Long).Show();
+						var intent = new Intent(Intent.ActionOpenDocumentTree);
+						intent.AddFlags(ActivityFlags.GrantWriteUriPermission);
+						StartActivity(intent);
+						if ((int)Build.VERSION.SdkInt >= (int)BuildVersionCodes.O)
+						{
+							await Task.Run(() => intent.PutExtra(DocumentsContract.ExtraInitialUri, "/sdcard/"));
+						}
 					}
 				}
-				var status = await Permissions.CheckStatusAsync<Permissions.StorageWrite>();
-				if (status == PermissionStatus.Granted)
-				{
-					return;
-				}
-				Toast.MakeText(this, "权限需要\", \"此软件需要权限以更改Config文件", ToastLength.Long).Show();
-				var request = await Permissions.RequestAsync<Permissions.StorageWrite>();
-
-				if (request == PermissionStatus.Granted)
-				{
-					return;
-				}
-				Toast.MakeText(this, "请到应用设置里授权：允许\"读取本机存储\"", ToastLength.Long).Show();
-				throw new Exception("应用权限不足");
+				else
+					Run();
 			}
-			catch
+			//var status = await Permissions.CheckStatusAsync<Permissions.StorageWrite>();
+			//if (status == PermissionStatus.Granted)
+			//{
+			//	return;
+			//}
+			//Toast.MakeText(this, "权限需要\", \"此软件需要权限以更改Config文件", ToastLength.Long).Show();
+			catch (Exception e)
 			{
-				Run();
+				Toast.MakeText(this, "应用权限不足-02", ToastLength.Long);
+				Log.Error(e);
+				throw new Exception("应用权限不足-02");
 			}
 		}
 		public void Run()
@@ -70,8 +83,6 @@ namespace SC4Android
 					AppDomain.CurrentDomain.Load(memoryStream.ToArray());
 				}
 			}
-			
-			
 			Program.Main();
 		}
 		public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
