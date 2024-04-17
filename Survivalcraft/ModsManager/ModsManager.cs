5da6298d27e61367168dff90f076747ef3306c52
把红色赋予黑海² <txs20061216@126.com>
2024-04-17 20:37:10 +0800

    修改版本为1.62A

--- Survivalcraft/ModsManager/ModsManager.cs
+++ Survivalcraft/ModsManager/ModsManager.cs
@@ -18,7 +18,7 @@ using System.IO;
 public static class ModsManager
 {
 	public const string modSuffix = ".scmod";
-	public const string APIVersion = "1.61X";
+	public const string APIVersion = "1.62A";
 	public const string SCVersion = "2.3.10.4";
 	//1为api1.33 2为api1.40
 	public const int Apiv = 10;
