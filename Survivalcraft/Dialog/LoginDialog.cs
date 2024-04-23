2aabd0bf3862664c617846690f8203b4dff19ec2
LX.. <1659404657@qq.com>
2024-04-23 13:03:00 +0800

    完全修复SchubExternalContentProvider类型报错

--- Survivalcraft/Dialog/LoginDialog.cs
+++ Survivalcraft/Dialog/LoginDialog.cs
@@ -1,85 +1,76 @@
-﻿using Engine;
+using Engine;
 using System;
 using System.Collections.Generic;
 using System.IO;
 
-namespace Game
-{
-	public class LoginDialog : Dialog
-	{
-		public Action<byte[]> succ;
-		public Action<Exception> fail;
-		public StackPanelWidget MainView;
-		public BevelledButtonWidget btna, btnb, btnc;
-		public TextBoxWidget txa, txb;
-		public BusyDialog busyDialog = new("提示", "登录中");
-		public LabelWidget tip = new() { HorizontalAlignment = WidgetAlignment.Near, VerticalAlignment = WidgetAlignment.Near, Margin = new Vector2(1f, 1f) };
-		public Action cancel;
-		public LoginDialog()
-		{
-			var canvasWidget = new CanvasWidget() { Size = new Vector2(600f, 240f), HorizontalAlignment = WidgetAlignment.Center, VerticalAlignment = WidgetAlignment.Center };
-			var rectangleWidget = new RectangleWidget() { FillColor = new Color(0, 0, 0, 255), OutlineColor = new Color(128, 128, 128, 128), OutlineThickness = 2 };
-			var stackPanelWidget = new StackPanelWidget() { Direction = LayoutDirection.Vertical, HorizontalAlignment = WidgetAlignment.Center, VerticalAlignment = WidgetAlignment.Near, Margin = new Vector2(10f, 10f) };
-			Children.Add(canvasWidget);
-			canvasWidget.Children.Add(rectangleWidget);
-			canvasWidget.Children.Add(stackPanelWidget);
-			MainView = stackPanelWidget;
-			MainView.Children.Add(tip);
-			MainView.Children.Add(makeTextBox("账号:"));
-			MainView.Children.Add(makeTextBox("密码:"));
-			MainView.Children.Add(makeButton());
+namespace Game {
+    public class LoginDialog : Dialog {
+        public Action<byte[]> succ;
+        public Action<Exception> fail;
+        public StackPanelWidget MainView;
+        public BevelledButtonWidget btna, btnb, btnc;
+        public TextBoxWidget txa, txb;
+        public BusyDialog busyDialog = new("提示", "登录中");
+        public LabelWidget tip = new() { HorizontalAlignment = WidgetAlignment.Near, VerticalAlignment = WidgetAlignment.Near, Margin = new Vector2(1f, 1f) };
+        public Action cancel;
+        public LoginDialog() {
+            var canvasWidget = new CanvasWidget() { Size = new Vector2(600f, 240f), HorizontalAlignment = WidgetAlignment.Center, VerticalAlignment = WidgetAlignment.Center };
+            var rectangleWidget = new RectangleWidget() { FillColor = new Color(0, 0, 0, 255), OutlineColor = new Color(128, 128, 128, 128), OutlineThickness = 2 };
+            var stackPanelWidget = new StackPanelWidget() { Direction = LayoutDirection.Vertical, HorizontalAlignment = WidgetAlignment.Center, VerticalAlignment = WidgetAlignment.Near, Margin = new Vector2(10f, 10f) };
+            Children.Add(canvasWidget);
+            canvasWidget.Children.Add(rectangleWidget);
+            canvasWidget.Children.Add(stackPanelWidget);
+            MainView = stackPanelWidget;
+            MainView.Children.Add(tip);
+            MainView.Children.Add(makeTextBox("账号:"));
+            MainView.Children.Add(makeTextBox("密码:"));
+            MainView.Children.Add(makeButton());
 
-		}
-		public Widget makeTextBox(string title)
-		{
-			var canvasWidget = new CanvasWidget() { Margin = new Vector2(10, 0) };
-			var rectangleWidget = new RectangleWidget() { FillColor = Color.Black, OutlineColor = Color.White, Size = new Vector2(float.PositiveInfinity, 80) };
-			var stack = new StackPanelWidget() { Direction = LayoutDirection.Horizontal };
-			var label = new LabelWidget() { HorizontalAlignment = WidgetAlignment.Near, VerticalAlignment = WidgetAlignment.Near, Text = title, Margin = new Vector2(1f, 1f) };
-			var textBox = new TextBoxWidget() { VerticalAlignment = WidgetAlignment.Center, HorizontalAlignment = WidgetAlignment.Stretch, Color = new Color(255, 255, 255), Margin = new Vector2(4f, 0f), Size = new Vector2(float.PositiveInfinity, 80) };
-			if (title == "账号:") txa = textBox;
-			if (title == "密码:") txb = textBox;
-			stack.Children.Add(label);
-			stack.Children.Add(textBox);
-			canvasWidget.Children.Add(rectangleWidget);
-			canvasWidget.Children.Add(stack);
-			return canvasWidget;
-		}
-		public Widget makeButton()
-		{
-			var stack = new StackPanelWidget() { Direction = LayoutDirection.Horizontal };
-			var bevelledButtonWidget1 = new BevelledButtonWidget() { Size = new Vector2(160, 60), Margin = new Vector2(4f, 0), Text = "登录" };
-			var bevelledButtonWidget2 = new BevelledButtonWidget() { Size = new Vector2(160, 60), Margin = new Vector2(4f, 0), Text = "注册" };
-			var bevelledButtonWidget3 = new BevelledButtonWidget() { Size = new Vector2(160, 60), Margin = new Vector2(4f, 0), Text = "取消" };
-			stack.Children.Add(bevelledButtonWidget1);
-			stack.Children.Add(bevelledButtonWidget2);
-			stack.Children.Add(bevelledButtonWidget3);
-			btna = bevelledButtonWidget1;
-			btnb = bevelledButtonWidget2;
-			btnc = bevelledButtonWidget3;
-			return stack;
-		}
-		public override void Update()
-		{
-			if (btna.IsClicked)
-			{
-				var par = new Dictionary<string, string>
-				{
-					{ "user", txa.Text },
-					{ "pass", txb.Text }
-				};
-				DialogsManager.ShowDialog(this, busyDialog);
-				WebManager.Post(SPMBoxExternalContentProvider.m_redirectUri + "/com/api/login", par, null, new MemoryStream(), new CancellableProgress(), succ, fail);
-			}
-			if (btnb.IsClicked)
-			{
-				WebBrowserManager.LaunchBrowser(SPMBoxExternalContentProvider.m_redirectUri + "/com/reg");
-			}
-			if (btnc.IsClicked)
-			{
-				cancel?.Invoke();
-			}
+        }
+        public Widget makeTextBox(string title) {
+            var canvasWidget = new CanvasWidget() { Margin = new Vector2(10, 0) };
+            var rectangleWidget = new RectangleWidget() { FillColor = Color.Black, OutlineColor = Color.White, Size = new Vector2(float.PositiveInfinity, 80) };
+            var stack = new StackPanelWidget() { Direction = LayoutDirection.Horizontal };
+            var label = new LabelWidget() { HorizontalAlignment = WidgetAlignment.Near, VerticalAlignment = WidgetAlignment.Near, Text = title, Margin = new Vector2(1f, 1f) };
+            var textBox = new TextBoxWidget() { VerticalAlignment = WidgetAlignment.Center, HorizontalAlignment = WidgetAlignment.Stretch, Color = new Color(255, 255, 255), Margin = new Vector2(4f, 0f), Size = new Vector2(float.PositiveInfinity, 80) };
+            if (title == "账号:") txa = textBox;
+            if (title == "密码:") txb = textBox;
+            stack.Children.Add(label);
+            stack.Children.Add(textBox);
+            canvasWidget.Children.Add(rectangleWidget);
+            canvasWidget.Children.Add(stack);
+            return canvasWidget;
+        }
+        public Widget makeButton() {
+            var stack = new StackPanelWidget() { Direction = LayoutDirection.Horizontal };
+            var bevelledButtonWidget1 = new BevelledButtonWidget() { Size = new Vector2(160, 60), Margin = new Vector2(4f, 0), Text = "登录" };
+            var bevelledButtonWidget2 = new BevelledButtonWidget() { Size = new Vector2(160, 60), Margin = new Vector2(4f, 0), Text = "注册" };
+            var bevelledButtonWidget3 = new BevelledButtonWidget() { Size = new Vector2(160, 60), Margin = new Vector2(4f, 0), Text = "取消" };
+            stack.Children.Add(bevelledButtonWidget1);
+            stack.Children.Add(bevelledButtonWidget2);
+            stack.Children.Add(bevelledButtonWidget3);
+            btna = bevelledButtonWidget1;
+            btnb = bevelledButtonWidget2;
+            btnc = bevelledButtonWidget3;
+            return stack;
+        }
+        public override void Update() {
+            if (btna.IsClicked) {
+                var par = new Dictionary<string, string>
+                {
+                    { "user", txa.Text },
+                    { "pass", txb.Text }
+                };
+                DialogsManager.ShowDialog(this, busyDialog);
+                WebManager.Post(SchubExternalContentProvider.m_redirectUri + "/com/api/login", par, null, new MemoryStream(), new CancellableProgress(), succ, fail);
+            }
+            if (btnb.IsClicked) {
+                WebBrowserManager.LaunchBrowser(SchubExternalContentProvider.m_redirectUri + "/com/reg");
+            }
+            if (btnc.IsClicked) {
+                cancel?.Invoke();
+            }
 
-		}
-	}
+        }
+    }
 }
--- Survivalcraft/Dialog/ViewGameLogDialog.cs
+++ Survivalcraft/Dialog/ViewGameLogDialog.cs
@@ -3,168 +3,137 @@ using Engine.Media;
 using System.IO;
 using System.Xml.Linq;
 using System.Text.Json.Nodes;
-namespace Game
-{
-	public class ViewGameLogDialog : Dialog
-	{
-		public ListPanelWidget m_listPanel;
+namespace Game {
+    public class ViewGameLogDialog : Dialog {
+        public ListPanelWidget m_listPanel;
 
-		public ButtonWidget m_copyButton, m_filterButton, m_closeButton, m_uploadButton, m_clearButton;
+        public ButtonWidget m_copyButton, m_filterButton, m_closeButton, m_uploadButton, m_clearButton;
 
-		public LogType m_filter;
+        public LogType m_filter;
 
-		public static string fName = "ViewGameLogDialog";
+        public static string fName = "ViewGameLogDialog";
 
-		public ViewGameLogDialog()
-		{
-			XElement node = ContentManager.Get<XElement>("Dialogs/ViewGameLogDialog");
-			LoadContents(this, node);
-			m_listPanel = Children.Find<ListPanelWidget>("ViewGameLogDialog.ListPanel");
-			m_clearButton = Children.Find<ButtonWidget>("ViewGameLogDialog.Clear");
-			m_copyButton = Children.Find<ButtonWidget>("ViewGameLogDialog.Copy");
-			m_filterButton = Children.Find<ButtonWidget>("ViewGameLogDialog.Filter");
-			m_filterButton.Style = ContentManager.Get<XElement>("Styles/ButtonStyle_160x60");
-			m_closeButton = Children.Find<ButtonWidget>("ViewGameLogDialog.Close");
-			m_uploadButton = Children.Find<ButtonWidget>("ViewGameLogDialog.Upload");
-			m_listPanel.ItemClicked += delegate (object item)
-			{
-				if (m_listPanel.SelectedItem == item)
-				{
-					DialogsManager.ShowDialog(ParentWidget, new MessageDialog("Log Item", item.ToString(), LanguageControl.Ok, null, null));
-				}
-			};
-			PopulateList();
-		}
+        public ViewGameLogDialog() {
+            XElement node = ContentManager.Get<XElement>("Dialogs/ViewGameLogDialog");
+            LoadContents(this, node);
+            m_listPanel = Children.Find<ListPanelWidget>("ViewGameLogDialog.ListPanel");
+            m_clearButton = Children.Find<ButtonWidget>("ViewGameLogDialog.Clear");
+            m_copyButton = Children.Find<ButtonWidget>("ViewGameLogDialog.Copy");
+            m_filterButton = Children.Find<ButtonWidget>("ViewGameLogDialog.Filter");
+            m_filterButton.Style = ContentManager.Get<XElement>("Styles/ButtonStyle_160x60");
+            m_closeButton = Children.Find<ButtonWidget>("ViewGameLogDialog.Close");
+            m_uploadButton = Children.Find<ButtonWidget>("ViewGameLogDialog.Upload");
+            m_listPanel.ItemClicked += delegate (object item) {
+                if (m_listPanel.SelectedItem == item) {
+                    DialogsManager.ShowDialog(ParentWidget, new MessageDialog("Log Item", item.ToString(), LanguageControl.Ok, null, null));
+                }
+            };
+            PopulateList();
+        }
 
-		public override void Update()
-		{
-			if (m_clearButton.IsClicked)
-			{
-				GameLogSink.m_stream.SetLength(0);
-				m_listPanel.ClearItems();
-			}
+        public override void Update() {
+            if (m_clearButton.IsClicked) {
+                GameLogSink.m_stream.SetLength(0);
+                m_listPanel.ClearItems();
+            }
 
-			if (m_copyButton.IsClicked) ClipboardManager.ClipboardString = GameLogSink.GetRecentLog(131072);
+            if (m_copyButton.IsClicked) ClipboardManager.ClipboardString = GameLogSink.GetRecentLog(131072);
 
-			if (m_filterButton.IsClicked)
-			{
-				if (m_filter < LogType.Warning)
-				{
-					m_filter = LogType.Warning;
-				}
-				else
-				{
-					m_filter = m_filter < LogType.Error ? LogType.Error : LogType.Debug;
-				}
-				PopulateList();
-			}
-			if (Input.Cancel || m_closeButton.IsClicked)
-			{
-				DialogsManager.HideDialog(this);
-			}
-			if (m_filter == LogType.Debug)
-			{
-				m_filterButton.Text = LanguageControl.Get("Usual", "All");
-			}
-			else if (m_filter == LogType.Warning)
-			{
-				m_filterButton.Text = LanguageControl.Get("Usual", "Warnings");
-			}
-			else if (m_filter == LogType.Error)
-			{
-				m_filterButton.Text = LanguageControl.Get("Usual", "Errors");
-			}
-			if (m_uploadButton.IsClicked)
-			{
-				if (string.IsNullOrEmpty(SettingsManager.ScpboxAccessToken))
-				{
-					var messageDialog = new MessageDialog(LanguageControl.Get(fName, 1), LanguageControl.Get(fName, 2), LanguageControl.Get(fName, 3), LanguageControl.Get(fName, 4), (btn) =>
-					{
-						DialogsManager.HideAllDialogs();
-					});
-					DialogsManager.ShowDialog(this, messageDialog);
-				}
-				else
-				{
-					var cancellableProgress = new CancellableProgress();
-					var dialog = new CancellableBusyDialog(LanguageControl.Get(fName, 5), true);
-					DialogsManager.ShowDialog(this, dialog);
-					var jsonObject = new JsonObject();
-					var dictionary = new Dictionary<string, string>();
-					jsonObject.Add("path", "/GameLog/" + DateTime.Now.Ticks + ".log");
-					dictionary.Add("Authorization", "Bearer " + SettingsManager.ScpboxAccessToken);
-					dictionary.Add("Content-Type", "application/octet-stream");
-					dictionary.Add("Dropbox-API-Arg", jsonObject.ToJsonString());
-					var memoryStream = new MemoryStream();
-					GameLogSink.m_stream.Seek(0, SeekOrigin.Begin);
-					GameLogSink.m_stream.CopyTo(memoryStream);
-					memoryStream.Seek(0, SeekOrigin.Begin);
-					WebManager.Post(SPMBoxExternalContentProvider.m_redirectUri + "/com/files/upload", null, dictionary, memoryStream, cancellableProgress, delegate
-					{
-						dialog.LargeMessage = LanguageControl.Get(fName, 6);
-						dialog.m_cancelButtonWidget.Text = "OK";
-						GameLogSink.m_writer.BaseStream.SetLength(0);
-						GameLogSink.m_writer.Flush();
-						PopulateList();
-					}, delegate (Exception error)
-					{
-						dialog.LargeMessage = LanguageControl.Get(fName, 7);
-						dialog.SmallMessage = error.Message;
-					});
-				}
-			}
-		}
+            if (m_filterButton.IsClicked) {
+                if (m_filter < LogType.Warning) {
+                    m_filter = LogType.Warning;
+                }
+                else {
+                    m_filter = m_filter < LogType.Error ? LogType.Error : LogType.Debug;
+                }
+                PopulateList();
+            }
+            if (Input.Cancel || m_closeButton.IsClicked) {
+                DialogsManager.HideDialog(this);
+            }
+            if (m_filter == LogType.Debug) {
+                m_filterButton.Text = LanguageControl.Get("Usual", "All");
+            }
+            else if (m_filter == LogType.Warning) {
+                m_filterButton.Text = LanguageControl.Get("Usual", "Warnings");
+            }
+            else if (m_filter == LogType.Error) {
+                m_filterButton.Text = LanguageControl.Get("Usual", "Errors");
+            }
+            if (m_uploadButton.IsClicked) {
+                if (string.IsNullOrEmpty(SettingsManager.ScpboxAccessToken)) {
+                    var messageDialog = new MessageDialog(LanguageControl.Get(fName, 1), LanguageControl.Get(fName, 2), LanguageControl.Get(fName, 3), LanguageControl.Get(fName, 4), (btn) => {
+                        DialogsManager.HideAllDialogs();
+                    });
+                    DialogsManager.ShowDialog(this, messageDialog);
+                }
+                else {
+                    var cancellableProgress = new CancellableProgress();
+                    var dialog = new CancellableBusyDialog(LanguageControl.Get(fName, 5), true);
+                    DialogsManager.ShowDialog(this, dialog);
+                    var jsonObject = new JsonObject();
+                    var dictionary = new Dictionary<string, string>();
+                    jsonObject.Add("path", "/GameLog/" + DateTime.Now.Ticks + ".log");
+                    dictionary.Add("Authorization", "Bearer " + SettingsManager.ScpboxAccessToken);
+                    dictionary.Add("Content-Type", "application/octet-stream");
+                    dictionary.Add("Dropbox-API-Arg", jsonObject.ToJsonString());
+                    var memoryStream = new MemoryStream();
+                    GameLogSink.m_stream.Seek(0, SeekOrigin.Begin);
+                    GameLogSink.m_stream.CopyTo(memoryStream);
+                    memoryStream.Seek(0, SeekOrigin.Begin);
+                    WebManager.Post(SchubExternalContentProvider.m_redirectUri + "/com/files/upload", null, dictionary, memoryStream, cancellableProgress, delegate {
+                        dialog.LargeMessage = LanguageControl.Get(fName, 6);
+                        dialog.m_cancelButtonWidget.Text = "OK";
+                        GameLogSink.m_writer.BaseStream.SetLength(0);
+                        GameLogSink.m_writer.Flush();
+                        PopulateList();
+                    }, delegate (Exception error) {
+                        dialog.LargeMessage = LanguageControl.Get(fName, 7);
+                        dialog.SmallMessage = error.Message;
+                    });
+                }
+            }
+        }
 
-		public void PopulateList()
-		{
-			m_listPanel.ItemWidgetFactory = delegate (object item)
-			{
-				string text = (item != null) ? item.ToString() : string.Empty;
-				Color color = Color.Gray;
-				if (text.Contains("ERROR:"))
-				{
-					color = Color.Red;
-				}
-				else if (text.Contains("WARNING:"))
-				{
-					color = Color.DarkYellow;
-				}
-				else if (text.Contains("INFO:"))
-				{
-					color = Color.LightGray;
-				}
-				return new LabelWidget
-				{
-					Text = text,
-					Font = BitmapFont.DebugFont,
-					HorizontalAlignment = WidgetAlignment.Near,
-					VerticalAlignment = WidgetAlignment.Center,
-					FontScale = 0.6f,
-					Color = color
-				};
-			};
-			List<string> recentLogLines = GameLogSink.GetRecentLogLines(131072);
-			m_listPanel.ClearItems();
-			if (recentLogLines.Count > 1000)
-			{
-				recentLogLines.RemoveRange(0, recentLogLines.Count - 1000);
-			}
-			foreach (string item in recentLogLines)
-			{
-				if (m_filter == LogType.Warning)
-				{
-					if (!item.Contains("WARNING:") && !item.Contains("ERROR:"))
-					{
-						continue;
-					}
-				}
-				else if (m_filter == LogType.Error && !item.Contains("ERROR:"))
-				{
-					continue;
-				}
-				m_listPanel.AddItem(item);
-			}
-			m_listPanel.ScrollPosition = m_listPanel.Items.Count * m_listPanel.ItemSize;
-		}
-	}
+        public void PopulateList() {
+            m_listPanel.ItemWidgetFactory = delegate (object item) {
+                string text = (item != null) ? item.ToString() : string.Empty;
+                Color color = Color.Gray;
+                if (text.Contains("ERROR:")) {
+                    color = Color.Red;
+                }
+                else if (text.Contains("WARNING:")) {
+                    color = Color.DarkYellow;
+                }
+                else if (text.Contains("INFO:")) {
+                    color = Color.LightGray;
+                }
+                return new LabelWidget {
+                    Text = text,
+                    Font = BitmapFont.DebugFont,
+                    HorizontalAlignment = WidgetAlignment.Near,
+                    VerticalAlignment = WidgetAlignment.Center,
+                    FontScale = 0.6f,
+                    Color = color
+                };
+            };
+            List<string> recentLogLines = GameLogSink.GetRecentLogLines(131072);
+            m_listPanel.ClearItems();
+            if (recentLogLines.Count > 1000) {
+                recentLogLines.RemoveRange(0, recentLogLines.Count - 1000);
+            }
+            foreach (string item in recentLogLines) {
+                if (m_filter == LogType.Warning) {
+                    if (!item.Contains("WARNING:") && !item.Contains("ERROR:")) {
+                        continue;
+                    }
+                }
+                else if (m_filter == LogType.Error && !item.Contains("ERROR:")) {
+                    continue;
+                }
+                m_listPanel.AddItem(item);
+            }
+            m_listPanel.ScrollPosition = m_listPanel.Items.Count * m_listPanel.ItemSize;
+        }
+    }
 }
