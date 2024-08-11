3ac132848aed4e39f8a763031ba93f615b5182bb
镁生素 <2094790738@qq.com>
2024-08-11 10:11:00 +0800

    为爆炸提供了接口，可以实现耐炸王掉落物、耐炸王实体、改变受爆炸影响的弹射物。不过仍然缺少爆炸生成弹射物、爆炸破坏方块的接口。

--- Survivalcraft/Subsystem/SubsystemExplosions.cs
+++ Survivalcraft/Subsystem/SubsystemExplosions.cs
@@ -553,50 +553,77 @@ namespace Game
 			foreach (ComponentBody body in m_subsystemBodies.Bodies)
 			{
 				CalculateImpulseAndDamage(body, null, out Vector3 impulse, out float damage);
-				impulse *= m_random.Float(0.5f, 1.5f);
-				damage *= m_random.Float(0.5f, 1.5f);
-				body.ApplyImpulse(impulse);
-				body.Entity.FindComponent<ComponentHealth>()?.Injure(damage, null, ignoreInvulnerability: false, "Blasted by explosion");
-				body.Entity.FindComponent<ComponentDamage>()?.Damage(damage);
-				ComponentOnFire componentOnFire = body.Entity.FindComponent<ComponentOnFire>();
-				if (componentOnFire != null && m_random.Float(0f, 1f) < MathUtils.Min(damage - 0.1f, 0.5f))
+				var components = body.Entity.FindComponents<IPostprocessExplosions>();
+				bool skipVanilla_ = false;
+				foreach (var component in components)
 				{
-					componentOnFire.SetOnFire(null, m_random.Float(6f, 8f));
+					bool skipVanilla = false;
+					component.OnExplosion(this, impulse, damage, out skipVanilla);
+					skipVanilla_ |= skipVanilla;
 				}
+				if(!skipVanilla_)
+				{
+                    impulse *= m_random.Float(0.5f, 1.5f);
+                    damage *= m_random.Float(0.5f, 1.5f);
+                    body.ApplyImpulse(impulse);
+                    body.Entity.FindComponent<ComponentHealth>()?.Injure(damage, null, ignoreInvulnerability: false, "Blasted by explosion");
+                    body.Entity.FindComponent<ComponentDamage>()?.Damage(damage);
+                    ComponentOnFire componentOnFire = body.Entity.FindComponent<ComponentOnFire>();
+                    if (componentOnFire != null && m_random.Float(0f, 1f) < MathUtils.Min(damage - 0.1f, 0.5f))
+                    {
+                        componentOnFire.SetOnFire(null, m_random.Float(6f, 8f));
+                    }
+                }
 			}
 			foreach (Pickable pickable in m_subsystemPickables.Pickables)
 			{
 				Block block2 = BlocksManager.Blocks[Terrain.ExtractContents(pickable.Value)];
 				CalculateImpulseAndDamage(pickable.Position + new Vector3(0f, 0.5f, 0f), 20f, null, out Vector3 impulse2, out float damage2);
-				if (damage2 / block2.GetExplosionResilience(pickable.Value) > 0.1f)
+				bool skipVanilla_ = false;
+                IPostprocessExplosions pickablePostprocessExplosions = pickable as IPostprocessExplosions;
+				if(pickablePostprocessExplosions != null)
 				{
-					TryExplodeBlock(Terrain.ToCell(pickable.Position.X), Terrain.ToCell(pickable.Position.Y), Terrain.ToCell(pickable.Position.Z), pickable.Value);
-					pickable.ToRemove = true;
+					pickablePostprocessExplosions.OnExplosion(this, impulse2, damage2, out skipVanilla_);
 				}
-				else
+				if(!skipVanilla_)
 				{
-					Vector3 vector = (impulse2 + new Vector3(0f, 0.1f * impulse2.Length(), 0f)) * m_random.Float(0.75f, 1f);
-					if (vector.Length() > 10f)
-					{
-						Projectile projectile = m_subsystemProjectiles.AddProjectile(pickable.Value, pickable.Position, pickable.Velocity + vector, m_random.Vector3(0f, 20f), null);
-						if (m_random.Float(0f, 1f) < 0.33f)
-						{
-							m_subsystemProjectiles.AddTrail(projectile, Vector3.Zero, new SmokeTrailParticleSystem(15, m_random.Float(0.75f, 1.5f), m_random.Float(1f, 6f), Color.White));
-						}
-						pickable.ToRemove = true;
-					}
-					else
-					{
-						pickable.Velocity += vector;
-					}
-				}
+                    if (damage2 / block2.GetExplosionResilience(pickable.Value) > 0.1f)
+                    {
+                        TryExplodeBlock(Terrain.ToCell(pickable.Position.X), Terrain.ToCell(pickable.Position.Y), Terrain.ToCell(pickable.Position.Z), pickable.Value);
+                        pickable.ToRemove = true;
+                    }
+                    else
+                    {
+                        Vector3 vector = (impulse2 + new Vector3(0f, 0.1f * impulse2.Length(), 0f)) * m_random.Float(0.75f, 1f);
+                        if (vector.Length() > 10f)
+                        {
+                            Projectile projectile = m_subsystemProjectiles.AddProjectile(pickable.Value, pickable.Position, pickable.Velocity + vector, m_random.Vector3(0f, 20f), null);
+                            if (m_random.Float(0f, 1f) < 0.33f)
+                            {
+                                m_subsystemProjectiles.AddTrail(projectile, Vector3.Zero, new SmokeTrailParticleSystem(15, m_random.Float(0.75f, 1.5f), m_random.Float(1f, 6f), Color.White));
+                            }
+                            pickable.ToRemove = true;
+                        }
+                        else
+                        {
+                            pickable.Velocity += vector;
+                        }
+                    }
+                }
+                
 			}
 			foreach (Projectile projectile2 in m_subsystemProjectiles.Projectiles)
 			{
 				if (!m_generatedProjectiles.ContainsKey(projectile2))
-				{
-					CalculateImpulseAndDamage(projectile2.Position + new Vector3(0f, 0.5f, 0f), 20f, null, out Vector3 impulse3, out float _);
-					projectile2.Velocity += (impulse3 + new Vector3(0f, 0.1f * impulse3.Length(), 0f)) * m_random.Float(0.75f, 1f);
+				{ 
+                    CalculateImpulseAndDamage(projectile2.Position + new Vector3(0f, 0.5f, 0f), 20f, null, out Vector3 impulse3, out float damage3);
+                    bool skipVanilla_ = false;
+                    IPostprocessExplosions projectilePostprocessExplosions = projectile2 as IPostprocessExplosions;
+                    if (projectilePostprocessExplosions != null)
+                    {
+                        projectilePostprocessExplosions.OnExplosion(this, impulse3, damage3, out skipVanilla_);
+                    }
+                    if(!skipVanilla_) projectile2.Velocity += (impulse3 + new Vector3(0f, 0.1f * impulse3.Length(), 0f)) * m_random.Float(0.75f, 1f);
 				}
 			}
 			var position = new Vector3(point.X, point.Y, point.Z);
