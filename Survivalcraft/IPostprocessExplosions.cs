c8eaccc258dd9b0e0dbc32a6503a614d2e93fe5f
镁生素 <2094790738@qq.com>
2024-08-11 10:11:00 +0800

    为爆炸提供了接口，可以实现耐炸王掉落物、耐炸王实体、改变受爆炸影响的弹射物。不过仍然缺少爆炸生成弹射物、爆炸破坏方块的接口。

---
+++ Survivalcraft/IPostprocessExplosions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine;
+
+namespace Game
+{
+    public interface IPostprocessExplosions
+    {
+        public void OnExplosion(SubsystemExplosions subsystemExplosions, Vector3 impulse, float damage, out bool skipVanilla);
+    }
+}
