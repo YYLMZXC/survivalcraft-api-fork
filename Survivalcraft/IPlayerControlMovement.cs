fd7a919f56fad56d4c1ccca5da507e3117608bc1
镁生素 <2094790738@qq.com>
2024-08-21 12:12:29 +0800

    对ComponentPlayer添加了大量接口，让Mod可以自定义输入操作

---
+++ Survivalcraft/IPlayerControlMovement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    public interface IPlayerControlMovement
+    {
+        public void OnPlayerControlSteed(ComponentPlayer componentPlayer, bool skippedByOtherMods, out bool skipVanilla)
+        {
+            skipVanilla = false;
+        }
+        public void OnPlayerControlBoat(ComponentPlayer componentPlayer, bool skippedByOtherMods, out bool skipVanilla) 
+        {
+            skipVanilla = false;
+        }
+
+        public void OnPlayerControlWalk(ComponentPlayer componentPlayer, bool skippedByOtherMods, out bool skipVanilla)
+        {
+            skipVanilla = false;
+        }
+    }
+}
