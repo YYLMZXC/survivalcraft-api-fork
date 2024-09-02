159d5b72a79609130747f8d45a472e58661fc7c6
镁生素 <2094790738@qq.com>
2024-09-02 21:10:44 +0800

    部分方块从Index改为BlockIndex

--- Survivalcraft/Block/FramedGlassBlock.cs
+++ Survivalcraft/Block/FramedGlassBlock.cs
@@ -5,7 +5,7 @@ namespace Game
 		public const int Index = 44;
 		public override bool ShouldGenerateFace(SubsystemTerrain subsystemTerrain, int face, int value, int neighborValue)
 		{
-			if (Terrain.ExtractContents(neighborValue) == Index) return false;
+			if (Terrain.ExtractContents(neighborValue) == BlockIndex) return false;
 			return base.ShouldGenerateFace(subsystemTerrain, face, value, neighborValue);
 		}
 	}
