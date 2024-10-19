6ffdd743fd25da45a09bc3a74da3785589bc2f8a
镁生素 <2094790738@qq.com>
2024-10-19 20:46:41 +0800

    安卓项目增加了Attackment 旧AttackBody增加了Null判断

--- Survivalcraft/Component/ComponentMiner.cs
+++ Survivalcraft/Component/ComponentMiner.cs
@@ -539,8 +539,8 @@ namespace Game
 		}
         public static void AttackBody(ComponentBody target, ComponentCreature attacker, Vector3 hitPoint, Vector3 hitDirection, float attackPower, bool isMeleeAttack)
         {
-			if (isMeleeAttack) AttackBody(new MeleeAttackment(target.Entity, attacker.Entity, hitPoint, hitDirection, attackPower));
-			else AttackBody(new ProjectileAttackment(target.Entity, attacker.Entity, hitPoint, hitDirection, attackPower));
+			if (isMeleeAttack) AttackBody(new MeleeAttackment(target?.Entity, attacker?.Entity, hitPoint, hitDirection, attackPower));
+			else AttackBody(new ProjectileAttackment(target?.Entity, attacker?.Entity, hitPoint, hitDirection, attackPower));
         }
 
 		public static void AddHitValueParticleSystem(float damage, Entity attacker, Entity attacked, Vector3 hitPoint, Vector3 hitDirection)
