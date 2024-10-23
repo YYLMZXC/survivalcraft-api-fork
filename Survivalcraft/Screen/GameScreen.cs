79eb6cdca04b0017302e47bd4ca651a6ac06baf5
镁生素 <2094790738@qq.com>
2024-10-23 19:26:12 +0800

    关闭窗口时会退出游戏，避免挂后台

--- Survivalcraft/Screen/GameScreen.cs
+++ Survivalcraft/Screen/GameScreen.cs
@@ -16,6 +16,7 @@ namespace Game
 			Window.Deactivated += delegate
 			{
 				GameManager.SaveProject(waitForCompletion: true, showErrorDialog: false);
+				GameManager.DisposeProject();
 			};
 		}
 
