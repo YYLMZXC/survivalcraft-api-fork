ffd2cff768f91d7a2a1b21f0a20d0626dec8b54c
镁生素 <2094790738@qq.com>
2024-10-26 18:20:23 +0800

    新建了Injury对象，便于在传递受伤信息的时候有更多可传递信息（比如是被近战攻击打伤还是魔法攻击打伤）

---
+++ Survivalcraft/Game/Injury.cs
@@ -0,0 +1,20 @@
+﻿using Engine;
+using GameEntitySystem;
+
+namespace Game
+{
+    public class Injury
+    {
+        public float Amount;
+        Attackment Attackment;
+        bool IgnoreInvulnerability;
+        string Cause;
+        ComponentCreature Attacker
+        {
+            get
+            {
+                return Attackment?.Attacker?.FindComponent<ComponentCreature>();
+            }
+        }
+    }
+}
