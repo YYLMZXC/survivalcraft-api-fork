a182ac5e5f0ee692dfe16c59a0e2b8879743dcfe
镁生素 <2094790738@qq.com>
2024-11-08 15:49:17 +0800

    修复无伤害远程攻击仍然会造成击晕和击退的bug

--- Survivalcraft/Game/ProjectileAttackment.cs
+++ Survivalcraft/Game/ProjectileAttackment.cs
@@ -8,10 +8,18 @@ namespace Game
         Projectile Projectile;
         public ProjectileAttackment(Entity target, Entity attacker, Vector3 hitPoint, Vector3 hitDirection, float attackPower, Projectile projectile)
             : base(target, attacker, hitPoint, hitDirection, attackPower)
-        {
-            ImpulseFactor = 2f;
-            StunTimeAdd = 0.2f;
-            Projectile = projectile;
+		{
+			Projectile = projectile;
+			if(CalculateInjuryAmount() == 0f && AttackPower > 0f)
+			{
+				ImpulseFactor = 2f;
+				StunTimeAdd = 0.2f;
+			}
+			else
+			{
+				ImpulseFactor = 0f;
+				StunTimeAdd = 0f;
+			}
         }
 
         public ProjectileAttackment(ComponentBody target, Entity attacker, Vector3 hitPoint, Vector3 hitDirection, float attackPower, Projectile projectile)
